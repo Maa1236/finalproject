fca7306606c0a52828330429867b6879f6e0ed98
jole994 <obradovicjovana347@gmail.com>
2021-09-09 18:53:45 +0200

    login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import {Services} from './Services/Services';
-import {Fragment} from 'react';
-import {Login} from './Login/Login'
+import {Fragment,useState} from 'react';
+import {Login} from './Components/Login/Login';
+import {MainPage} from './Components/MainPage/MainPage';
 
 function App() {
+
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+
+  async function letMeIn(event) {
+    event.preventDefault();
+    let data = await Services(inputEmail, inputPassword);
+    console.log(data);
+  }
+
+ 
   return (
     <Fragment>
-      <Login />
-     
+      <Login 
+      setInputEmail={setInputEmail}
+      setInputPassword={setInputPassword}
+      letMeIn={letMeIn}
+      />
+      <MainPage />
     </Fragment>
     
    
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,46 @@
+import "./Login.css";
+
+export const Login = ({ setInputEmail, setInputPassword,letMeIn }) => {
+    
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <div className="login-form bg-light mt-4 p-4">
+            <form action="" method="">
+              <h4 className="text-center">Login</h4>
+              <div className="col-12 row g-3 nasaKlasa">
+                <input
+                  type="text"
+                  name="email"
+                  className="form-control"
+                  placeholder="email"
+                  onChange={(event) => { setInputEmail(event.target.value) } }
+                  />
+              </div>
+              <div className="col-12 row g-3 nasaKlasa">
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  placeholder="password"
+                  onChange={(event) => { setInputPassword(event.target.value) } }
+                  />
+              </div>
+              <div className="col-12 row g-3 nasaKlasa">
+                <button type="submit" className="btn btn-dark " onClick={letMeIn} >
+                  Login
+                </button>
+              </div>
+              <div className="col-12 row g-3 nasaKlasa">
+                <button type="reset" className="btn btn-dark ">
+                  Reset
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
