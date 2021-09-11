9bb3e5ba6c4358e937adb767691782671c93445d
Sladjana1992 <slax01@live.de>
2021-09-11 11:11:42 +0200

    task 4 finished

--- src/Components/CandidateCard/CandidateCard.jsx
+++ src/Components/CandidateCard/CandidateCard.jsx
@@ -1,10 +1,15 @@
 import { Candidates } from '../Candidates/Candidates';
 import './CandidateCard.css';
 
-export const CandidateCard = ({ candidate }) => {
+export const CandidateCard = ({ candidate, setLeadToReport }) => {
+
+    const showMeUserReport = () => {
+        setLeadToReport(true);
+    }
+
     return (
-        <div className="singleElUserCard">
-            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="bla" key={candidate.id} />
+        <div className="singleElUserCard" key={candidate.id} onClick={showMeUserReport}>
+            <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="bla" />
             <p className="firstName">{candidate.name} </p>
             <div className="info">
                 <p>{candidate.email} </p>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Header.css";
 
-export const Header = () => {
+export const Header = ({setLeadToReport}) => {
+
+  const backToMainPage = () => {
+    setLeadToReport(false);
+  }
+
   return (
     <nav className="navbar navbar-dark bg-dark peddingNav">
       <div className="container-fluid">
         <h1 className="navbar-brand">Interviews Reports</h1>
         <form className="d-flex">
-          <button className="btn btn-light" type="button">
+          <button className="btn btn-light" type="button" onClick={backToMainPage}>
             Candidates
           </button>
         </form>
