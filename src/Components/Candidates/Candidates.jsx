b3ef5159873eab5458253af2d30063e1b5bf2069
IvanaPaunovic88 <paunovic.ivana.arh@gmail.com>
2021-09-13 22:06:11 +0200

    Task 9 finished

--- src/Components/Candidates/Candidates.jsx
+++ src/Components/Candidates/Candidates.jsx
@@ -7,24 +7,38 @@ import { Fragment } from "react/cjs/react.production.min";
 
 export const Candidates = ({ setIsLogIn, setLeadToReport, candidates, setCatchId }) => {
 
+    const [search, setSearchTerm] = useState("");
+    console.log(candidates)
     if (candidates === "jwt expired") {
         localStorage.clear()
         setIsLogIn(false);
         window.location.reload(true)
     }
+    let mappingTheCandidates = candidates.filter((candid) => {
+        let result = null;
+        if (search === "") {
+            result = candid;
+        } else if (candid.name.toLowerCase().includes(search.toLowerCase())) {
+            result = candid;
+        } return result;
+    }).map((candidate, index) => {
 
-    let candidateComponent = candidates.map((candidate) => {
         return (
-            <CandidateCard setCatchId= {setCatchId} candidate={candidate} key={candidate.id} setLeadToReport={setLeadToReport} />
-        )
-    })
+
+            <CandidateCard
+                key={index}
+                setCatchId={setCatchId}
+                candidate={candidate} />
+        );
+    });
 
     return (
         <Fragment>
-            <Search />
+            <Search setSearchTerm={setSearchTerm} />
             <div className="containerMain">
-                {candidateComponent}
+                {mappingTheCandidates}
             </div>
+
         </Fragment>
     )
 }
